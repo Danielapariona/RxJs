b413393699a99a53c645f6ac4410f95b15287b58
Danielapariona <daniela.parionac@gmail.com>
2021-02-27 22:34:00 -0500

    Add 01-observables

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,7 @@
-import { Observable, Observer } from 'rxjs';
+import { Observer } from "rxjs";
 
 const observer: Observer<any> = {
     next: value => console.log('siguiente', value),
     error: error => console.warn('error [obs]', error),
     complete: () => console.log('Completado [obs]')
 }
-
-const obs$ = new Observable<string>(subs => {
-    subs.next('Hola');
-    subs.next('Mundo');
-
-    subs.next('Hola');
-    subs.next('Mundo');
-
-    // Error
-    /* const a = undefined;
-    a.nombre = 'Daniela'; */
-
-    subs.complete();
-
-    subs.next('Hola');
-    subs.next('Mundo');
-});
-
-obs$.subscribe(observer);
-/* obs$.subscribe(
-    valor => console.log('next', valor),
-    error => console.warn('error', error),
-    () => console.info('completado')
-);
- */
-
-
