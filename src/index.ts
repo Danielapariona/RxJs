fd83765d631b026a52860d9ad51ed99597a166f9
Danielapariona <daniela.parionac@gmail.com>
2021-03-01 00:48:00 -0500

    Subject

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Observable, Observer } from 'rxjs';
+import { Observable, Observer, Subject } from 'rxjs';
 
 const observer: Observer<any> = {
     next: value => console.log('next:', value),
@@ -6,44 +6,32 @@ const observer: Observer<any> = {
     complete: () => console.info('completado')
 };
 
+const intervalo$ = new Observable<number>(subs => {
+    const intervalId = setInterval(() =>
+        subs.next(Math.random()), 1000
+    );
 
-const intervalo$ = new Observable<number>(subscriber => {
-
-    // Crear un contador, 1,2,3,4,5,......
-    let count = 0;
-
-    const interval = setInterval(() => {
-        // cada segundo
-        count++;
-        subscriber.next(count);
-        console.log(count);
-
-    }, 1000);
-
-    setTimeout(() => {
-        subscriber.complete();
-    }, 2500);
-
-    return () => {
-        clearInterval(interval);
-        console.log('Intérvalo destruido');
-    }
-
+    return () => clearInterval(intervalId);
 });
 
 
-const subs1 = intervalo$.subscribe(observer);
-const subs2 = intervalo$.subscribe(observer);
-const subs3 = intervalo$.subscribe(observer);
+/**
+ * 1. Casteo multiple: Muchas suscripciones van a estar sujetas a este mismo subject y sirve para distrubir la misma información a todos los lugares que estén suscritos.
+ * 2. Tambien es un Observer
+ * 3. Next(), error, complete
+ */
+const subject$ = new Subject();
+intervalo$.subscribe(subject$);
 
-subs1.add(subs2)
-    .add(subs3);
 
+// const subs1 = intervalo$.subscribe((rnd) => console.log('subs1', rnd));
+// const subs2 = intervalo$.subscribe((rnd) => console.log('subs2', rnd));
+
+const subs1 = subject$.subscribe((rnd) => console.log('subs1', rnd));
+const subs2 = subject$.subscribe((rnd) => console.log('subs2', rnd));
 
 setTimeout(() => {
-    subs1.unsubscribe()
-    // subs2.unsubscribe()
-    // subs3.unsubscribe()
+    subject$.next(10); // Permite ingresar un datos al flujo de datos que es esta emitiendo el observable.
+    subject$.complete();
+}, 3500);
 
-    console.log('Completado timeout');
-}, 6000);
